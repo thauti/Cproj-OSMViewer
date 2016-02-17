baed7ba296babfad1a1f0c09881f508bf29b906f
HAUTIER thomas <hautier@lucien.informatique.univ-paris-diderot.fr>
2016-02-17 10:42:46 +0100

    Premier Test fenetrage

--- main.c
+++ main.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <gtk/gtk.h>
 
 #include "osm.h"
 #include "parse.h"
+void activate (GtkApplication* app,
+          gpointer        user_data)
+{
+  GtkWidget *window;
 
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_widget_show_all (window);
+}
 int main(int argc, char* argv)
 {
+	GtkApplication *app;
+	 int status;
 	char* filename = "example/map.osm";
 	printf("Ouverture de %s \n", filename);
 
+ app = gtk_application_new ("org.gtk.osm", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+
+	  status = g_application_run (G_APPLICATION (app), argc, argv);
+	  g_object_unref (app);
 	tree_node* abr = malloc(sizeof(tree_node));
 	tree_way* abr3 = malloc(sizeof(tree_way));
 	
@@ -16,7 +33,6 @@ int main(int argc, char* argv)
 	{
 		fprintf(stderr, "Erreur à l'ouverture de  %s \n", filename);
 	}
-
 	printf("# %p <- \n",getNodeById(3257359455, abr));
 	printf("# %p <- \n",getWayById(4039117, abr3));
 
