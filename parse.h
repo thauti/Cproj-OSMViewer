e88e51c2a2f6b9c73720e2add6b15a5bf7d97ec5
thom <thom0294@gmail.com>
2016-02-17 21:13:52 +0100

    Suppression vieux fichiers

--- parse.h
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <libxml/parser.h>
-#include <libxml/tree.h>
-
-
-void debugNode(node* n);
-void parsetoabr(xmlNode* node, tree_node* rootN, tree_way* rootW);
-int parse(char* filename, tree_node* abrN, tree_way* abrW);
