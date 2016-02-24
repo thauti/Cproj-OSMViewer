cab08f10c9ea799d4e7a9c36b28081d6004b1e56
HAUTIER thomas <hautier@informatique.univ-paris-diderot.fr>
2016-02-24 22:07:33 +0000

    Suppression Warnings typage + makefile

--- sources/parse.c
+++ sources/parse.c
@@ -18,6 +18,7 @@ void debugNode(node* n)
 }
 void parsetoabr(xmlNode* nodeX, map* usermap)
 {
+                printf("Erreur 1");
 
     usermap->nodes = malloc(sizeof(tree_node));
     usermap->ways = malloc(sizeof(tree_way));
@@ -26,29 +27,30 @@ void parsetoabr(xmlNode* nodeX, map* usermap)
     tree_node* rootN = usermap->nodes;
     tree_way* rootW = usermap->ways;
 
-	xmlNode* curr = nodeX->xmlChildrenNode;
-	node* no = malloc(sizeof(node));
-	way* wa = malloc(sizeof(way));
+    xmlNode* curr = nodeX->xmlChildrenNode;
+    node* no = malloc(sizeof(node));
+    way* wa = malloc(sizeof(way));
     bound* bo = malloc(sizeof(bound));
-	curr = curr->next;
-	while(curr != NULL)
-	{
-		if (curr->type == XML_ELEMENT_NODE) {
-            if(strcmp(curr->name, "bounds") == 0)
+    curr = curr->next;
+    while(curr != NULL)
+    {
+        if (curr->type == XML_ELEMENT_NODE) {
+            if(xmlStrcmp(curr->name, xmlCharStrdup("bounds")) == 0)
             {
-                usermap->bounds->minlat = atof(xmlGetProp(curr, "minlat"));
-                usermap->bounds->maxlat = atof(xmlGetProp(curr, "maxlat"));
-                usermap->bounds->maxlon = atof(xmlGetProp(curr, "maxlon"));
-                usermap->bounds->minlon = atof(xmlGetProp(curr, "minlon"));
+                usermap->bounds->minlat = atof((char*)xmlGetProp(curr, xmlCharStrdup("minlat")));
+                usermap->bounds->maxlat = atof((char*)xmlGetProp(curr, xmlCharStrdup("maxlat")));
+                usermap->bounds->maxlon = atof((char*)xmlGetProp(curr, xmlCharStrdup("maxlon")));
+                usermap->bounds->minlon = atof((char*)xmlGetProp(curr, xmlCharStrdup("minlon")));
 
             }
-			 if(strcmp(curr->name, "node") == 0){
+             if(xmlStrcmp(curr->name,  xmlCharStrdup("node")) == 0){
 
-        		no = malloc(sizeof(node));
-        		no->id = atoll(xmlGetProp(curr, "id"));
-        		no->lat = atof(xmlGetProp(curr, "lat"));
-        		no->longi = atof(xmlGetProp(curr, "lon"));
-                if(!strcmp(xmlGetProp(curr, "visible"), "true"))
+                no = malloc(sizeof(node));
+        		no->id = atoll((char*)xmlGetProp(curr, xmlCharStrdup("id")));
+        		no->lat = atof((char*)xmlGetProp(curr, xmlCharStrdup("lat")));
+        		no->longi = atof((char*)xmlGetProp(curr, xmlCharStrdup("lon")));
+                 printf("Erreur 2");
+                if(!xmlStrcmp(xmlGetProp(curr, xmlCharStrdup("visible")), xmlCharStrdup("true")))
                 {
                     no->visible = 1;
                 }
@@ -59,11 +61,12 @@ void parsetoabr(xmlNode* nodeX, map* usermap)
         		//debugNode(no);
         		insertNode(no, rootN);
          	}
-         	if(strcmp(curr->name, "way") == 0){
+         	if(xmlStrcmp(curr->name, xmlCharStrdup("way")) == 0){
 
         		wa = malloc(sizeof(way));
-        		wa->id = atoll(xmlGetProp(curr, "id"));
-                if(!strcmp(xmlGetProp(curr, "visible"), "true"))
+                wa->id = malloc(sizeof(int64_t));
+        		wa->id = atoll((char*)xmlGetProp(curr, xmlCharStrdup("id")));
+                if(!xmlStrcmp(xmlGetProp(curr, xmlCharStrdup("visible")), xmlCharStrdup("true")))
                 {
                     wa->visible = 1;
                 }
@@ -79,7 +82,7 @@ void parsetoabr(xmlNode* nodeX, map* usermap)
         		{
         			if (nd->type == XML_ELEMENT_NODE) {
         				//printf("%s \n",  nd->name);
-        				if(strcmp(nd->name, "nd") ==0)
+        				if(xmlStrcmp(nd->name, xmlCharStrdup("nd")) ==0)
         				{
     	    				n_tag++;
 	        			}
@@ -93,10 +96,10 @@ void parsetoabr(xmlNode* nodeX, map* usermap)
         		while(nd != NULL)
         		{
         			if (nd->type == XML_ELEMENT_NODE) {
-        				if(strcmp(nd->name, "nd") == 0)
+        				if(xmlStrcmp(nd->name, xmlCharStrdup("nd")) == 0)
         				{
     	    				
-    	    				wa->nodes[i] = getNodeById(atoll(xmlGetProp(nd, "ref")),rootN);
+    	    				wa->nodes[i] = getNodeById(atoll((char*)xmlGetProp(nd, xmlCharStrdup("ref"))),rootN);
 	        				
 	        				
 	        				i++;
@@ -116,7 +119,7 @@ void parsetoabr(xmlNode* nodeX, map* usermap)
 	{
 		 if (curr->type == XML_ELEMENT_NODE) {
 		 	printf("node type: Element, name: %s\n", curr->name);
-            if(strcmp(curr->name, "node") == 0){
+            if(xmlStrcmp(curr->name, "node") == 0){
 
         		no = malloc(sizeof(node));
         		no->id = atoll(xmlGetProp(curr, "id"));
@@ -127,7 +130,7 @@ void parsetoabr(xmlNode* nodeX, map* usermap)
         		//debugNode(no);
         		insertNode(no, rootN);
          	}
-         	if(strcmp(curr->name, "way") == 0){
+         	if(xmlStrcmp(curr->name, "way") == 0){
 
         		wa = malloc(sizeof(way));
         		wa->id = atoll(xmlGetProp(curr, "id"));
@@ -142,6 +145,7 @@ void parsetoabr(xmlNode* nodeX, map* usermap)
 
 int parse(char* filename, map* usermap)
 {
+
 	LIBXML_TEST_VERSION
 	xmlDocPtr doc;
 	xmlNode *root_element;
