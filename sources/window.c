4219fc6b400d2171bdb15af022c6f81de0f9c540
HAUTIER thomas <hautier@informatique.univ-paris-diderot.fr>
2016-04-16 20:00:16 +0200

    Projection 1

--- sources/window.c
+++ sources/window.c
@@ -9,7 +9,9 @@
 
 #define PI 3.141
 #define DEG2RAD(DEG) ((DEG)*((PI)/(180.0)))
-
+#define deg2rad(d) (((d)*M_PI)/180)
+#define rad2deg(d) (((d)*180)/M_PI)
+#define earth_radius 6378137
 
 double tomercator_x(double src)
 {
@@ -21,6 +23,18 @@ double tomercator_y(double src)
 
     return ;
 }
+double y2lat_d(double y) { return rad2deg(2 * atan(exp(  deg2rad(y) ) ) - M_PI/2); }
+double x2lon_d(double x) { return x; }
+double lat2y_d(double lat) { return rad2deg(log(tan(M_PI/4+ deg2rad(lat)/2))); }
+double lon2x_d(double lon) { return lon; }
+
+/* The following functions take or return their results in something close to meters, along the equator */
+
+double y2lat_m(double y) { return rad2deg(2 * atan(exp( (y / earth_radius ) )) - M_PI/2); }
+double x2lon_m(double x) { return rad2deg(x / earth_radius); }
+double lat2y_m(double lat) { return earth_radius * log(tan(M_PI/4+ deg2rad(lat)/2)); }
+double lon2x_m(double lon) { return deg2rad(lon) * earth_radius; }
+
 void dessiner_abr(GtkWidget* widget, cairo_t *cr, tree_way* t, bound* b)
 {
 
@@ -56,8 +70,8 @@ void dessiner_abr(GtkWidget* widget, cairo_t *cr, tree_way* t, bound* b)
 
                   //  double x_max = 800*((b->maxlon+180)/360)*zoom;
                  // double y_max = 600/2-log(tan((PI/4)+ DEG2RAD(b->maxlat)/2))*ratio*zoom;
-                    double xratio = (800/(b->maxlon-b->minlon));
-                    double yratio = (600/(b->maxlat-b->minlat))*-1;
+                    double xratio = (800/(lon2x_d(b->maxlon)-lon2x_d(b->minlon)));
+                    double yratio = (800/(lat2y_d(b->maxlat)-lat2y_d(b->minlat)))*-1;
         for(i=0;i<t->w->nodes_size-1;i++)
         {
             if(t->w->type_way != 0 && t->w->type_val != 0){
@@ -65,9 +79,12 @@ void dessiner_abr(GtkWidget* widget, cairo_t *cr, tree_way* t, bound* b)
                 if(t->w->visible == 1 ){
                     //cairo_line_to(cr, (t->w->nodes[i]->lat - b->minlat)*80000+100, (t->w->nodes[i]->longi - b->minlon)*80000-20);
                     
-                    double x = (t->w->nodes[i]->longi*xratio)-(b->minlon*xratio);
-                    double y = (t->w->nodes[i]->lat*yratio)-(b->minlat*yratio)+600;
-                    
+                 //   double x = (lon2x_d(t->w->nodes[i]->longi)*xratio)-(lon2x_d(b->minlon)*xratio);
+                //    double y = (lat2y_d(t->w->nodes[i]->lat)*yratio)-(lat2y_d(b->minlat)*yratio)+600;
+                    double xmin = (800/PI)*zoom*(deg2rad(b->minlon)+PI);
+                    double ymin = (800/PI)*zoom*(PI-log(tan((PI/4) + (deg2rad(b->minlat)/2))));
+                    double x = (800/PI)*zoom*(deg2rad(t->w->nodes[i]->longi)+PI)-xmin;
+                    double y = (800/PI)*zoom*(PI-log(tan((PI/4) + (deg2rad(t->w->nodes[i]->lat)/2))))-ymin+600;
                    // g_print("b->maxlon - b->monlon %lf \n", b->maxlon-b->minlon);
                     //g_print("b->maxlat - b->monlat %lf \n", b->maxlat-b->minlat);
                     //g_print("res : %f \n", (800/(b->maxlat-b->minlat)*b->maxlat) -  (800/(b->maxlat-b->minlat)*b->minlat));
@@ -78,8 +95,8 @@ void dessiner_abr(GtkWidget* widget, cairo_t *cr, tree_way* t, bound* b)
                     //g_print(" Ym : %f \n", y);
 
                     cairo_line_to(cr,x,y);
-                     g_print(" Xmx : %f \n", DEG2RAD(x));
-                    g_print(" Ymx : %f \n", 6371*log(tan(PI/4)+(DEG2RAD(y))/2));
+                     g_print(" Xmx : %f \n", x);
+                    g_print(" Ymx : %f \n", y);
                 }
             }
         }
